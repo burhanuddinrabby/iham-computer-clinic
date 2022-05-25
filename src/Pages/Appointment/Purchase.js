e0459b6d3811b6bf8f67bc98c858eba546984709
Burhan Uddin <bunnub5683@gmail.com>
2022-05-25 16:56:07 +0600

    order connected to server

--- src/Pages/Appointment/Purchase.js
+++ src/Pages/Appointment/Purchase.js
@@ -1,55 +1,71 @@
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import auth from '../../firebase.init';
 import Loading from '../Shared/Loading';
 
 const Purchase = () => {
     const { id } = useParams();
     const [user, loading] = useAuthState(auth);
-    const [errorMessage, setErrorMessage] = useState('');
     const [productQuantity, setQuantity] = useState(0);
     const [service, setService] = useState({});
     useEffect(() => {
-        async function fecthing() {
+        async function fetchData() {
             const res = await fetch(`http://localhost:5000/purchase/${id}`);
             const data = await res.json();
             setService(data);
             setQuantity(data.minQuantity);
         }
-        fecthing();
+        fetchData();
     }, [id]);
     const { name, price, description, img, minQuantity, available } = service;
-    // console.log(minQuantity);
-    // setQuantity(minQuantity);
-    // console.log(productQuantity);
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => {
-        console.log(data);
-        const newQuantity = parseInt(data.quantity);
-        // console.log(data.quantity);
-        if (newQuantity < minQuantity) {
-            setErrorMessage('You can not buy less than ' + minQuantity + ' ' + name + 's')
-        } else if (minQuantity > available) {
-            setErrorMessage('We are sorry we can\'t server you ' + available + ' ' + name + 's')
-        } else if (newQuantity > available) {
-            setErrorMessage('You can not buy more than ' + available + ' ' + name + 's')
-        } else {
-            setErrorMessage('');
-            const newAvailable = available - newQuantity;
-            setService({ ...service, available: newAvailable });
-            setQuantity(newQuantity)
-            const newData = { ...data, quantity: newQuantity }
-            sendToServer(newData);
+    const onSubmit = e => {
+        e.preventDefault();
+        const buyerName = e.target.name.value;
+        const email = e.target.email.value;
+        const address = e.target.address.value;
+        const phoneNumber = e.target.phoneNumber.value;
+        const quantity = parseInt(e.target.quantity.value);
+        const totalPrice = price * quantity;
+        const newOrder = { name, buyerName, email, address, phoneNumber, quantity, totalPrice };
+        if (quantity > available) {
+            toast.warn('Please check your order quantity, we only have ' + available + ' left');
+        } else if (quantity < minQuantity) {
+            toast.warn('Please check your order quantity, you have to order more than ' + minQuantity);
+        }
+        else if (newOrder.address !== '' && newOrder.phoneNumber !== '' && quantity !== 0) {
+            const confirm = window.confirm(`Are you sure you want to purchase this product for $${totalPrice} ?`);
+            if (confirm) {
+                const newAvailable = available - quantity;
+                const newService = { ...service, available: newAvailable };
+                setService(newService);
+                console.log(newOrder);
+                fetch('http://localhost:5000/orders', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(newOrder)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.success) {
+                            toast(`Your order is set. Go to dashboard and check My order`)
+                        }
+                        else {
+                            toast.error(`Failed to add order`)
+                        }
+                        // setTreatment(null);
+                        // refetch();
+                    });
+            }
         }
     };
-    const sendToServer = (newData) => {
-        console.log('sending', newData);
+    const handleOnChange = e => {
+        const quantity = parseInt(e.target.value);
+        setQuantity(quantity);
     }
-    // const handleChange = (e) => {
-    //     console.log('changing');
-    // }
     loading && <Loading></Loading>
     return (
         <div>
@@ -78,14 +94,14 @@ const Purchase = () => {
                                 </p>
 
 
-                                <form onSubmit={handleSubmit(onSubmit)}>
+                                <form onSubmit={onSubmit}>
                                     <div className="mt-4">
                                         <input
                                             className="border border-gray-200 bg-gray-200 p-4 rounded w-full text-base leading-4 text-[black]"
                                             type="text"
                                             readOnly
                                             value={user?.displayName}
-                                            {...register("name")}
+                                            name="name"
                                         />
                                     </div>
                                     <div className="mt-4">
@@ -94,7 +110,7 @@ const Purchase = () => {
                                             type="email"
                                             readOnly
                                             value={user?.email}
-                                            {...register("email")}
+                                            name="email"
                                         />
                                     </div>
 
@@ -103,32 +119,16 @@ const Purchase = () => {
                                             className=" border rounded-bl border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600"
                                             type="text"
                                             placeholder="Address"
-                                            {...register("address", {
-                                                required: {
-                                                    value: true,
-                                                    message: "Please enter your address"
-                                                }
-                                            })}
+                                            name="address"
+                                            required
                                         />
-                                        <label className="label pb-0">
-                                            {errors.address?.type === 'required' && <span className="label-text-alt text-red-500">{errors.address.message}</span>}
-                                        </label>
-
-
                                         <input
-                                            className=" border rounded-br border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600"
+                                            className="mt-4 border rounded-br border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600"
                                             type="number"
                                             placeholder="Phone Number"
-                                            {...register("phoneNumber", {
-                                                required: {
-                                                    value: true,
-                                                    message: "Please enter your phone number"
-                                                }
-                                            })}
+                                            name="phoneNumber"
+                                            required
                                         />
-                                        <label className="label">
-                                            {errors.phoneNumber?.type === 'required' && <span className="label-text-alt text-red-500">{errors.phoneNumber.message}</span>}
-                                        </label>
 
                                     </div>
 
@@ -143,14 +143,8 @@ const Purchase = () => {
                                                     className="border border-gray-300 p-3 w-full text-base leading-4 placeholder-gray-600 text-gray-600"
                                                     type="number"
                                                     value={productQuantity}
-                                                    onChange={() => console.log('changing')}
-                                                    readOnly={false}
-                                                    {...register("quantity", {
-                                                        required: {
-                                                            value: true,
-                                                            message: `Please put the amount you need. Not more than ${available} or less than ${minQuantity}`
-                                                        }
-                                                    })}
+                                                    onChange={handleOnChange}
+                                                    name="quantity"
                                                 />
                                                 <span className='btn btn-primary rounded-l-none font-bold text-2xl' onClick={() => setQuantity(productQuantity + 1)} disabled={productQuantity >= available}>+</span>
                                             </div>
@@ -158,39 +152,11 @@ const Purchase = () => {
                                     </div>
                                     <button
                                         type="submit"
-                                        disabled={minQuantity > available}
-                                        className="mt-8 border border-transparent text-white flex justify-center items-center py-4 rounded w-full">
-                                        <label htmlFor="my-modal-3" className="w-full btn modal-button">
-                                            <div>
-                                                <p className="text-white leading-4"> Place Order</p>
-
-                                            </div>
-                                        </label>
+                                        className="mt-8 border border-transparent text-white flex justify-center items-center py-4 rounded w-full btn">
+                                        Place Order
                                     </button>
                                 </form>
-                                <input type="checkbox" id="my-modal-3" className="modal-toggle" />
-                                <div className="modal">
-                                    <div className="modal-box relative">
-                                        <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                                        <h1>
-                                            {
-                                                ((errors.phoneNumber?.type === 'required') || (errors.address?.type === 'required')) ? <span className="label-text-alt text-red-500 text-xl">Please fill up the input field correctly. Those are required</span>
-                                                    :
-                                                    errorMessage ? <div>
-                                                        <p className='font-bold text-red-500 mb-3'>{errorMessage}</p>
-                                                        <p>Please make sure you're buying a valid amount of products</p>
-                                                    </div>
-                                                        : <div>
-                                                            <p className='font-bold mb-3 mt-3'>Total Price: ${productQuantity * price}</p>
-                                                            <p>You've to pay ${productQuantity * price} to buy {productQuantity} {name}s</p>
-                                                            <button className='btn-sm btn mt-4 btn-primary text-[#000000]'>Add to order</button>
-                                                            &nbsp; &nbsp;
-                                                            <label htmlFor="my-modal-3" className="btn btn-sm btn-error">Cancel</label>
-                                                        </div>
-                                            }
-                                        </h1>
-                                    </div>
-                                </div>
+
                             </div>
                         </div>
                     </div>
